db8b9b869256d771392a1e1b6e642bc7964aebf7
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-04-24 13:03:29 +0700

    Added Providers to Application

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import "../styles/globals.css";
+import Providers from '../components/common/Providers';
+import '../styles/globals.css';
 
 function Application({ Component, pageProps }) {
-  // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout || ((page) => page);
+    // Use the layout defined at the page level, if available
+    const getLayout = Component.getLayout || ((page) => page);
 
-  return <>{getLayout(<Component {...pageProps} />)}</>;
+    return <Providers>{getLayout(<Component {...pageProps} />)}</Providers>;
 }
 
 export default Application;
