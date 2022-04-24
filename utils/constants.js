181e29e356f003fb7b5b693f3c1ab8ce9209f87c
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-04-24 14:56:55 +0700

    Temporarily change API gateway to polygon test network

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = 'https://candle.vercel.app';
-export const API_URL = 'https://network.cndlchain.com';
+export const API_URL = 'https://rpc.poa.psdk.io:8545';
 
 export const APP_VERSION = '0.1.0';
