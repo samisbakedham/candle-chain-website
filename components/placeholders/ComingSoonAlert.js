fb7a6266021c7051c85e0b26185f8164c7194690
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-04-28 10:33:40 +0700

    Updated the Coming soon alert widget

--- components/placeholders/ComingSoonAlert.js
+++ components/placeholders/ComingSoonAlert.js
@@ -4,18 +4,12 @@ export default function ComingSoonAlert() {
             <div className="pt-32 w-2/3 lg:w-1/2 text-center text-white">
                 <div>
                     <h1 className="text-4xl font-bold">
-                        <span>This page will be available soon.</span>
+                        <span>Coming soon.</span>
                     </h1>
-                    <p className="mt-3 text-lg text-gray-300">
-                        Candle is committed to building a scalable, efficient
-                        place for developers to build on web3 and to make it
-                        easy for anyone to get started.
-                        <br />
-                        <br />
-                        <span className="text-indigo-300">
-                            We are currently working on this page.
-                        </span>
-                    </p>
+                    <h2 className="mt-2 font-semibold text-lg text-indigo-300">
+                        This page will be available soon and will be updated
+                        regularly.
+                    </h2>
                 </div>
             </div>
         </div>
