86db101f57d57f23dfc2aecada231a68e7e5d88a
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-04-28 10:32:09 +0700

    Added custom Coming soon alert

---
+++ components/placeholders/ComingSoonAlert.js
@@ -0,0 +1,23 @@
+export default function ComingSoonAlert() {
+    return (
+        <div className="w-full flex items-center justify-center">
+            <div className="pt-32 w-2/3 lg:w-1/2 text-center text-white">
+                <div>
+                    <h1 className="text-4xl font-bold">
+                        <span>This page will be available soon.</span>
+                    </h1>
+                    <p className="mt-3 text-lg text-gray-300">
+                        Candle is committed to building a scalable, efficient
+                        place for developers to build on web3 and to make it
+                        easy for anyone to get started.
+                        <br />
+                        <br />
+                        <span className="text-indigo-300">
+                            We are currently working on this page.
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
