8160383165bbd83139b051f646bc788857f68dde
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-04-28 12:12:57 +0700

    Updated Wallets Page content

--- pages/wallets/index.js
+++ pages/wallets/index.js
@@ -1,10 +1,75 @@
-import DefaultLayout from '../../components/layouts/DefaultLayout';
-import ComingSoonAlert from '../../components/placeholders/ComingSoonAlert';
+import { SunIcon } from '@heroicons/react/outline';
+import { WalletsOnboardLayout } from '../../components/layouts/WalletsLayout';
 
 WalletsPage.getLayout = (page) => {
-    return <DefaultLayout>{page}</DefaultLayout>;
+    return <WalletsOnboardLayout>{page}</WalletsOnboardLayout>;
 };
 
+const features = [
+    {
+        title: 'Candle Dashboard',
+        description:
+            'View your crypto assets and transactions in a simple and intuitive way.',
+        color: 'green',
+    },
+    {
+        title: 'Candle Wallet',
+        description:
+            'A wallet that allows you to send and receive crypto assets in a secure way.',
+        color: 'blue',
+    },
+    {
+        title: 'Candle Bridge',
+        description:
+            'Deposit and withdraw funds from your wallet to other networks.',
+        color: 'purple',
+    },
+    {
+        title: 'Candle Staking',
+        description: 'Stake your crypto assets to earn rewards.',
+        color: 'orange',
+    },
+];
+
 export default function WalletsPage() {
-    return <ComingSoonAlert></ComingSoonAlert>;
+    const generateFeatures = () => {
+        return features.map((feature) => (
+            <div
+                key={feature.title}
+                className="flex h-64 bg-zinc-700/70 hover:bg-zinc-700 hover:-translate-y-2 p-4 rounded-lg flex-col items-center justify-between transition duration-300"
+            >
+                <div className="text-center text-white font-bold text-2xl">
+                    {feature.title}
+                </div>
+
+                <SunIcon className="my-8 w-20 h-20 text-yellow-200" />
+
+                <div className="text-center h-1/2 text-base text-zinc-300">
+                    {feature.description}
+                </div>
+            </div>
+        ));
+    };
+
+    return (
+        <div className="p-8">
+            <div className="flex flex-col items-center justify-center md:flex-row space-y-2 md:space-y-0 md:space-x-4">
+                <div className="text-center text-2xl lg:text-4xl">
+                    Getting started with
+                </div>
+                <div className="px-4 py-2 text-center font-semibold text-lg md:text-2xl rounded-full bg-green-500/20 text-green-300">
+                    Candle Wallets
+                </div>
+            </div>
+
+            <div className="mt-2 text-center text-lg text-zinc-300">
+                The safe, fast, and secure way to send and receive crypto assets
+                on Candle Chain Network.
+            </div>
+
+            <div className="mt-8 md:mt-16 lg:mt-32 mx-auto max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                {generateFeatures()}
+            </div>
+        </div>
+    );
 }
